39ac1e98d828b4452d38669a20a616421b3e6f08
LAPTOP <caobinhen6@gmail.com>
2017-12-01 17:51:29 +0800

    版本原因，写法不同，修复一波

--- src/app/hero/hero.service.ts
+++ src/app/hero/hero.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 
-import { HttpClient, HttpHeaders } from '@angular/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import 'rxjs/add/operator/toPromise';
 
