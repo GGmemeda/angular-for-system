76191b94b2d0209282c673d91462c5fda5a7089d
LAPTOP <caobinhen6@gmail.com>
2017-12-06 16:39:55 +0800

    增加material2 ui框架

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MyOwnCustomMaterialModule} from './material.module'
+import 'hammerjs';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
@@ -23,7 +26,8 @@ import { MessagesComponent }    from './messages/messages.component';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-
+    NoopAnimationsModule,
+    MyOwnCustomMaterialModule,
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
     // Remove it when a real server is ready to receive requests.
---
+++ src/app/material.module.ts
@@ -0,0 +1,8 @@
+import {NgModule} from '@angular/core';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+
+@NgModule({
+  exports: [MatButtonModule, MatCheckboxModule]
+})
+export class MyOwnCustomMaterialModule {
+}
